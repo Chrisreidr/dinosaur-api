186ed8d08287ca98a6d79a0fdd869ca820fb4271
ChrisRodriguez <chrisreidr@gmail.com>
2022-06-02 20:58:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const dinosaurs = {
+    'tyrannosaurus': {
+        'name': 'Tyrannosaurus',
+        'timePeriod': 'Jurassic',
+        'diet': 'Carnivore'
+    },
+    'quetzalcoatlus': {
+        'name': 'Quetzalcoatlus',
+        'timePeriod': 'Cretaceous',
+        'diet': 'Carnivore'
+    },
+    'dreadnoughtus': {
+        'name': 'Dreadnoughtus',
+        'timePeriod': 'Cretaceous',
+        'diet': 'Herbivore'
+    },
+    'unknown': {
+        'name': 'unknown',
+        'timePeriod': 'unknown',
+        'diet': 'unknown'
+    }
+}
+
+// request
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req,res) => {
+    const dinoName = req.params.name.toLowerCase();
+    if(dinosaurs[dinoName]){
+        res.json(dinosaurs[dinoName]);
+    } else {
+        res.json(dinosaurs['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The sever is running on port: ${PORT}. Better go catch it!`);
+})
